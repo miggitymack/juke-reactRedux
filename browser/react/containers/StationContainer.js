3c04fff44d88a766d626337a9fbe06fa4d3e6140
Jason <jason.miguel@gmail.com>
2017-04-03 16:40:36 -0400

    finished genre lists

--- browser/react/containers/StationContainer.js
+++ browser/react/containers/StationContainer.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import Station from '../components/Station';
-import store from '../store'
+import store from '../store';
+import {convertSong} from '../utils'
+import {toggleSong} from '../action-creators/player'
 
-
-
-// const convertSongsToStations = function (songsArray){
-//   let stations = {};
-//   for (let i = 0; i < songsArray.length; i++) {
-//     if (stations[songsArray[i].genre]) {
-//       stations[songsArray[i].genre].push(songsArray[i])
-//     } else {
-//       stations[songsArray[i].genre] = [songsArray[i]];
-//     }
-//   }
-//   return stations;
-// }
-
-const mapStateToProps = function (state){
-  return {}
+const convertStation = function (songsArray){
+  let stations = {};
+  for (let i = 0; i < songsArray.length; i++) {
+    if (stations[songsArray[i].genre]) {
+      stations[songsArray[i].genre].push(songsArray[i])
+    } else {
+      stations[songsArray[i].genre] = [songsArray[i]];
+    }
+  }
+  return stations;
 }
 
-const mapDispatchToProps = function (dispatch){
-  return {};
+const mapStateToProps = function (state, ownProps) {
+  console.log(ownProps);
+  return {
+    genreName: ownProps.params.genreName,
+    songs: state.songs.filter(song => song.genre === ownProps.params.genreName)
+      .map(convertSong),
+    currentSong: state.player.currentSong,
+    isPlaying: state.player.isPlaying
+  }
+}
+
+const mapDispatchToProps = function (dispatch, ownProps){
+  return {
+    toggleOne: function (song, list) {
+      dispatch(toggleSong(song, list))
+    }
+  };
 }
 
 const StationContainer = connect(
